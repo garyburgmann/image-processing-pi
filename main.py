d969d3520b9e3c83a08a92606cd7974763e96144
Gary Burgmann <garyburgmann@gmail.com>
2020-07-12 14:55:22 +1000

    cleanup ObjectDetection class. use dynamic label allocation. call bash script to download model/labels to /tmp if not already present

--- main.py
+++ main.py
@@ -1,85 +1,64 @@
+""" module to use ObjectDetection class with pre-captured video files """
 import time
 import os
 import argparse
 import json
 import copy
 
-import requests
+# import requests
 import cv2
+import numpy as np
 
-import pedestrian_detection_pi as pdp
+from object_detection import ObjectDetection
+
+od = ObjectDetection()
 
 
-def run(**kwargs):
-    frame = kwargs['frame']
-    filename = kwargs['filename']
+def run(img: np.ndarray):
     img = kwargs['img']
 
-    # p.o.c. running DO droplet with 2 GB memory, 1 vCPU
-    if kwargs['endpoint'] == 'api':
-        # using api hosted with digital ocean
-        res = requests.post(
-            'https://pd.garyburgmann.com/classify',
-            json=img.tolist()
-        )
-        # print(res.json())
-        res.raise_for_status()
-        res = res.json()['result']
-    else:
-        # tf lite model from local module
-        res = pdp.run(img)
+    res = od.exec(img)
 
     bbox_list = []
     conf_list = []
-    if res:
-        bbox_list = [x['bounding_box'] for x in res]
-        print('boxe(s) found: {}'.format(bbox_list))
-        conf_list = [x['score'] for x in res]
 
     CAMERA_HEIGHT, CAMERA_WIDTH = img.shape[:2]
-    for idx, (ymin, xmin, ymax, xmax) in enumerate(bbox_list):
-        # ymin, xmin, ymax, xmax = box
-        xmin = int(xmin * CAMERA_WIDTH)
-        xmax = int(xmax * CAMERA_WIDTH)
-        ymin = int(ymin * CAMERA_HEIGHT)
-        ymax = int(ymax * CAMERA_HEIGHT)
-        confidence = conf_list[idx]
 
-        # draw bounding box
-        cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (125, 255, 51), 2)
-        # draw label above bounding box
-        cv2.putText(
-            img,
-            f'Person | {confidence:.2f}',
-            (xmin, ymin - 20),
-            cv2.FONT_HERSHEY_COMPLEX,
-            0.5,
-            (0, 255, 0),
-            2
-        )
+    if res:
+        for bbox in res:
+            ymin, xmin, ymax, xmax = bbox['bounding_box']
 
-        # with open(f'mot17_06/results_{filename}.txt', 'a+') as f:
-        #     f.write(
-        #         f'{frame},-1,{xmin},{ymin},{xmax - xmin},{ymax - ymin},'
-        #         f'{confidence}\n'
-        #     )
+            xmin = int(xmin * CAMERA_WIDTH)
+            xmax = int(xmax * CAMERA_WIDTH)
+            ymin = int(ymin * CAMERA_HEIGHT)
+            ymax = int(ymax * CAMERA_HEIGHT)
+
+            confidence = bbox['score']
+            label = bbox['class']
+
+            # draw bounding box
+            cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (125, 255, 51), 2)
+            # draw label above bounding box
+            cv2.putText(
+                img,
+                f'{label.capitalize()} | {confidence:.2f}',
+                (xmin, ymin - 20),
+                cv2.FONT_HERSHEY_COMPLEX,
+                0.5,
+                (0, 255, 0),
+                2
+            )
 
     if True:
         cv2.imshow('img', img[:, :, ::-1])
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run people detection')
+    parser = argparse.ArgumentParser(description='Run object detection')
     parser.add_argument(
         '-v',
         type=str,
         help='path to video file'
-    )
-    parser.add_argument(
-        '-e',
-        type=str,
-        help='api or local',
-        default='local'
     )
 
     args = parser.parse_args()
@@ -94,7 +73,7 @@ if __name__ == '__main__':
         ret, cv2_im = cap.read()
         if ret is True:
             frame = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
-            run(img=frame, frame=num_frames, filename=start, endpoint=args.e)
+            run(img=frame, frame=num_frames, endpoint=args.e)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
--- object_detection.py
+++ object_detection.py
@@ -1,50 +1,95 @@
-"""Example using TF Lite to detect objects with the Raspberry Pi camera."""
+""" Use TF Lite to detect objects """
 import argparse
 import io
 import re
 import time
+import subprocess
+import os
+from typing import List, Dict
 
 import numpy as np
 # import picamera
-
 from PIL import Image
 import tensorflow as tf
-
-CAMERA_WIDTH = 640
-CAMERA_HEIGHT = 480
 
 
 class ObjectDetection:
     _input_details: None
     _output_details: None
+    _model_download_script = './download_model.sh'
 
-    def __init__(self, interpreter_path: str, threshold: float = 0.6):
+    def __init__(
+        self,
+        interpreter_path: str = '/tmp/detect.tflite',
+        labels_path: str = '/tmp/coco_labels.txt',
+        target_label: str = '__all__',
+        threshold: float = 0.6
+    ):
         """ ObjectDetection constructor
 
         interpreter_path: path to .tflite file
         """
-        self._interpreter = tf.lite.Interpreter(interpreter_path)
+        self._interpreter_path = interpreter_path
+        self._labels_path = labels_path
+        self._target_label = target_label
+        self._threshold = threshold
+        self._validate_interpreter_and_labels_paths()
+        self._interpreter = self._bootstrap_interpreter()
         self._prepare_interpreter()
+        self._labels = self._load_labels()
 
-    def _prepare_interpreter(self):
+    def _validate_interpreter_and_labels_paths(self) -> None:
+        if not (
+            os.path.exists(self._interpreter_path)
+            and os.path.exists(self._labels_path)
+        ):
+            _ = subprocess.call(self._model_download_script, shell=True)
+        if not (
+            os.path.exists(self._interpreter_path)
+            and os.path.exists(self._labels_path)
+        ):
+            raise Exception('missing interpeter and/or labels')
+
+    def _bootstrap_interpreter(self) -> tf.lite.Interpreter:
+        return tf.lite.Interpreter(self._interpreter_path)
+
+    def _prepare_interpreter(self) -> None:
         # https://www.tensorflow.org/lite/guide/inference
         self._interpreter.allocate_tensors()
         self._input_details = self._interpreter.get_input_details()
         self._output_details = self._interpreter.get_output_details()
 
-    def _set_input_tensor(self, image):
+    def _load_labels(self) -> Dict:
+        """
+        Loads the labels file.
+        Supports files with or without index numbers.
+        """
+        with open(self._labels_path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            labels = {}
+            for row_number, content in enumerate(lines):
+                pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
+                if len(pair) == 2 and pair[0].strip().isdigit():
+                    labels[int(pair[0])] = pair[1].strip()
+                else:
+                    labels[row_number] = pair[0].strip()
+        return labels
+
+    def _set_input_tensor(self, image: Image) -> None:
         """ Sets the input tensor. """
         tensor_index = self._input_details[0]['index']
+        # A function that can return a new numpy array pointing to the internal
+        # TFLite tensor state at any point.
         input_tensor = self._interpreter.tensor(tensor_index)()[0]
         input_tensor[:, :] = image
 
-    def _get_output_tensor(self, index):
+    def _get_output_tensor(self, index: int) -> np.ndarray:
         """ Returns the output tensor at the given index. """
         output_index = self._output_details[index]['index']
         tensor = np.squeeze(self._interpreter.get_tensor(output_index))
         return tensor
 
-    def _detect_objects(image):
+    def _detect_objects(self, image: Image) -> List[Dict]:
         """
         Returns a list of detection results, each a dictionary of object
         info
@@ -60,125 +105,36 @@ class ObjectDetection:
 
         results = []
         for i in range(count):
-            if scores[i] >= self._threshold:
+            label = self._labels[classes[i]].lower()
+            target = self._target_label.lower()
+            valid_label = target in [label, '__all__']
+            if scores[i] >= self._threshold and valid_label:
                 result = {
                     'bounding_box': boxes[i],
-                    'class_id': classes[i],
+                    'class': label,
                     'score': scores[i]
                 }
                 results.append(result)
         return results
 
-    def run(self, img_input):
-        try:
-            _, input_height, input_width, _ = self._input_details[0]['shape']
-            image = (
-                Image.fromarray(img_input)
-                .resize(
-                    (input_width, input_height),
-                    Image.ANTIALIAS
-                )
+    def _load_image(self, img_input: np.ndarray) -> Image:
+        _, input_height, input_width, _ = self._input_details[0]['shape']
+        return (
+            Image.fromarray(img_input)
+            .resize(
+                (input_width, input_height),
+                Image.ANTIALIAS
             )
-            start_time = time.monotonic()
+        )
+
+    def exec(self, img_input: np.ndarray) -> List[Dict]:
+        try:
+            image = self._load_image(img_input)
+            # start_time = time.monotonic()
             results = self._detect_objects(image)
-            elapsed_ms = (time.monotonic() - start_time) * 1000
+            # elapsed_ms = (time.monotonic() - start_time) * 1000
         except Exception as exc:
             print(str(exc))
             results = []
         finally:
             return results
-
-
-# def load_labels(path):
-#     """Loads the labels file. Supports files with or without index numbers."""
-#     with open(path, 'r', encoding='utf-8') as f:
-#         lines = f.readlines()
-#         labels = {}
-#         for row_number, content in enumerate(lines):
-#             pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
-#             if len(pair) == 2 and pair[0].strip().isdigit():
-#                 labels[int(pair[0])] = pair[1].strip()
-#             else:
-#                 labels[row_number] = pair[0].strip()
-#     return labels
-
-# def set_input_tensor(interpreter, image):
-#     """Sets the input tensor."""
-#     tensor_index = interpreter.get_input_details()[0]['index']
-#     input_tensor = interpreter.tensor(tensor_index)()[0]
-#     input_tensor[:, :] = image
-
-
-# def get_output_tensor(interpreter, index):
-#     """Returns the output tensor at the given index."""
-#     output_details = interpreter.get_output_details()[index]
-#     tensor = np.squeeze(interpreter.get_tensor(output_details['index']))
-#     return tensor
-
-
-# def detect_objects(interpreter, image, threshold):
-#     """Returns a list of detection results, each a dictionary of object info."""
-#     set_input_tensor(interpreter, image)
-#     interpreter.invoke()
-
-#     # Get all output details
-#     boxes = get_output_tensor(interpreter, 0)
-#     classes = get_output_tensor(interpreter, 1)
-#     scores = get_output_tensor(interpreter, 2)
-#     count = int(get_output_tensor(interpreter, 3))
-
-#     results = []
-#     for i in range(count):
-#         if scores[i] >= threshold:
-#             result = {
-#                 'bounding_box': boxes[i],
-#                 'class_id': classes[i],
-#                 'score': scores[i]
-#             }
-#             results.append(result)
-#     return results
-
-
-# def run(img_array):
-#     # interpreter = tf.lite.Interpreter('/tmp/detect.tflite')
-#     # threshold = 0.6
-#     # interpreter.allocate_tensors()
-#     # _, input_height, input_width, _ = \
-#     #     interpreter.get_input_details()[0]['shape']
-
-# #   with picamera.PiCamera(
-# #       resolution=(CAMERA_WIDTH, CAMERA_HEIGHT), framerate=30) as camera:
-# #     camera.start_preview()
-#     try:
-#     #   stream = io.BytesIO()
-#     #   annotator = Annotator(camera)
-#     #   for _ in camera.capture_continuous(
-#     #       stream, format='jpeg', use_video_port=True):
-#     #     stream.seek(0)
-#         image = (
-#             Image.fromarray(img_array)
-#             .resize(
-#                 (input_width, input_height),
-#                 Image.ANTIALIAS
-#             )
-#         )
-#         # image = Image.open(stream).convert('RGB').resize(
-#         #     (input_width, input_height), Image.ANTIALIAS)
-#         start_time = time.monotonic()
-#         results = detect_objects(interpreter, image, threshold)
-#         elapsed_ms = (time.monotonic() - start_time) * 1000
-#         return results
-#         # annotator.clear()
-#         # annotate_objects(annotator, results, labels)
-#         # annotator.text([5, 0], '%.1fms' % (elapsed_ms))
-#         # annotator.update()
-
-#         # stream.seek(0)
-#         # stream.truncate()
-
-#     finally:
-#         pass
-#     #   camera.stop_preview()
-
-# # if __name__ == '__main__':
-# #   main()
