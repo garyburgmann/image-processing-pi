789b6a141d5a2965a956b58c9591aabe1f3987e9
Gary Burgmann <garyburgmann@gmail.com>
2020-08-14 13:53:53 +1000

    fix pi env script

--- bin/pi_bootstrap_venv.sh
+++ bin/pi_bootstrap_venv.sh
@@ -3,5 +3,5 @@ VENV_DIR=/tmp
 VENV=${VENV_DIR}/.venv
 
 python3 -m venv ${VENV} --clear
-${VENV}/pip install --upgrade pip setuptools
+${VENV}/bin/pip install --upgrade pip setuptools
 ${VENV}/bin/pip install -r pi.requirements.txt
