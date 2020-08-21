ac5fc6c1cad2361de259b5d85b88614c03058870
Gary Burgmann <garyburgmann@gmail.com>
2020-08-22 00:01:12 +1000

    add script for pi full tensorflow install 2.3.0

---
+++ bin/pi_install_tensorflow.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+VENV_DIR=${PWD}
+VENV=${VENV_DIR}/.venv
+cd /tmp
+wget https://github.com/PINTO0309/Tensorflow-bin/master/tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh
+chmod +x tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh
+./tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh
+${VENV}/bin/pip install tensorflow-2.3.0-cp37-none-linux_armv7l.whl
