05d2f81bebb4a97e62c2dda101e196622b42c1f6
Gary Burgmann <garyburgmann@gmail.com>
2020-08-22 00:03:53 +1000

    fix typo in pi tensorflow install script

--- bin/pi_install_tensorflow.sh
+++ bin/pi_install_tensorflow.sh
@@ -2,7 +2,7 @@
 VENV_DIR=${PWD}
 VENV=${VENV_DIR}/.venv
 cd /tmp
-wget https://github.com/PINTO0309/Tensorflow-bin/master/tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh
+wget https://github.com/PINTO0309/Tensorflow-bin/raw/master/tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh
 chmod +x tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh
 ./tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh
 ${VENV}/bin/pip install tensorflow-2.3.0-cp37-none-linux_armv7l.whl
