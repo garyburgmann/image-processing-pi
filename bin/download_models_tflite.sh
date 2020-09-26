062e92354d82e7998546de7883a06b6bea872976
Gary Burgmann <garyburgmann@gmail.com>
2020-09-26 15:57:36 +1000

    add start_client script and update model download script

--- bin/download_models_tflite.sh
+++ bin/download_models_tflite.sh
@@ -12,11 +12,18 @@ mkdir -p $MODELS_DIR
 
 cd $MODELS_DIR
 
-# as referenced - https://www.tensorflow.org/lite/models/object_detection/overview
-# https://tfhub.dev/tensorflow/tfjs-model/ssd_mobilenet_v1/1/default/1
-MODEL_URI="https://tfhub.dev/tensorflow/lite-model/ssd_mobilenet_v1/1/metadata/2?lite-format=tflite"
+# # as referenced - https://www.tensorflow.org/lite/models/object_detection/overview
+# # https://tfhub.dev/tensorflow/tfjs-model/ssd_mobilenet_v1/1/default/1
+# MODEL_URI="https://tfhub.dev/tensorflow/lite-model/ssd_mobilenet_v1/1/metadata/2?lite-format=tflite"
 
-curl -O $MODEL_URI  # ssd_mobilenet_v1_1_default_1.tflite
+# curl -O $MODEL_URI  # ssd_mobilenet_v1_1_default_1.tflite
+
+# includes labels, but they are obselete
+MODEL_FILE="coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip"
+curl -O "http://storage.googleapis.com/download.tensorflow.org/models/tflite/$MODEL_FILE"
+unzip -o $MODEL_FILE
+rm $MODEL_FILE
+rm labelmap.txt
 
 # as referenced - https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf1_detection_zoo.md
 MODEL_FILE="ssd_mobilenet_v3_large_coco_2020_01_14.tar.gz"
